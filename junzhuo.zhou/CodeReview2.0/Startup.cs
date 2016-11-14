f2c4b234c71edd919e44d4a2ed96fb91062ffe55
zhoujunzhuo <750907624@qq.com>
2016-11-14 17:54:13 +0800

    submit
    submit

---
+++ junzhuo.zhou/CodeReview2.0/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeReview.Startup))]
+namespace CodeReview
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
